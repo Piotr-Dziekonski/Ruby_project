7b2f754c5d58b70e35761abe200130cb24a47583
Piotr Dziekoński <piotrdziekon@gmail.com>
2016-07-02 00:35:34 +0200

    done 06 assigment

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+	def add_zero (input)
+			if input < 10
+				input = '0' + input.to_s 
+			end
+			input.to_s
+	end
+
+  def time_string
+	hours = @seconds / 3600
+	minutes = (@seconds % 3600) / 60
+	seconds = @seconds % 60
+
+	"#{add_zero(hours)}:#{add_zero(minutes)}:#{add_zero(seconds)}"	
+  end
 end
