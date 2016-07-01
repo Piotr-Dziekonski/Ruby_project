f601cf76bc452ed3bcd69ba77e52d339e551bf75
Piotr Dziekoński <piotrdziekon@gmail.com>
2016-07-01 23:54:43 +0200

    done 05 assigment

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title
+		input = @title.split
+		input.each do |word|
+			input[0].capitalize!
+			if word != "over" && word != "and" && word != "the" && word != "of" && word != "in" && word != "a" && word != "an"
+				word.capitalize!
+			end
+		end
+		return input.join(" ")
+	end
 end
